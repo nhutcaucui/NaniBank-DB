05e537a3f26c2c91817d7647ed9db3769911ad27
nvnam <vinam@intra.co.kr>
2020-05-28 12:02:54 +0700

    changed the type field of partner to hashMethod due to it is no longer being used

--- partner/partner-db-product.sql
+++ partner/partner-db-product.sql
@@ -8,6 +8,6 @@ CREATE TABLE partner (
     id int(10) NOT NULL AUTO_INCREMENT,
     name varchar(32),
     publicKey varchar(512),
-    type varchar(10),
+    hashMethod varchar(128),
     PRIMARY KEY (id)
 );
